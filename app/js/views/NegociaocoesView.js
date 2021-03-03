99ab7eaa48971b2e0e6b2f09457e9ead8905170e
juniormoraes <contatomoraesjr@gmail.com>
2021-03-03 09:02:23 -0300

    responsabilidade visualização  tabela  em uma view

---
+++ app/js/views/NegociaocoesView.js
@@ -0,0 +1,36 @@
+class NegociacoesView {
+    constructor(seletor) {
+        this._elemento = document.querySelector(seletor);
+    }
+    update(modelo) {
+        this._elemento.innerHTML = this.template(modelo);
+    }
+    template(model) {
+        return `
+    <table class="table table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th>DATA</th>
+                    <th>QUANTIDADE</th>
+                    <th>VALOR</th>
+                    <th>VOLUME</th>
+                </tr>
+            </thead>
+
+            <tbody>
+              ${model.paraArray().map(negociacao => `
+                    <tr>
+                        <th>${negociacao.data.getDate()}/${negociacao.data.getMonth() + 1}/${negociacao.data.getFullYear()}</th>
+                        <th>${negociacao.quantidade}</th>
+                        <th>${negociacao.valor}</th>
+                        <th>${negociacao.volume}</th>
+                    </tr>
+                  `).join()}
+            </tbody>
+
+            <tfoot>
+            </tfoot>
+        </table>
+    `;
+    }
+}
---
+++ app/ts/views/NegociaocoesView.ts
@@ -0,0 +1,45 @@
+class NegociacoesView {
+
+  private _elemento: Element;
+
+  constructor(seletor: string){
+    this._elemento = document.querySelector(seletor);
+  
+  }
+
+  update(modelo: Negociacoes): void{
+    this._elemento.innerHTML = this.template(modelo);
+
+  }
+
+  template(model: Negociacoes): string {
+    return `
+    <table class="table table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th>DATA</th>
+                    <th>QUANTIDADE</th>
+                    <th>VALOR</th>
+                    <th>VOLUME</th>
+                </tr>
+            </thead>
+
+            <tbody>
+              ${model.paraArray().map(negociacao=>
+                  `
+                    <tr>
+                        <th>${negociacao.data.getDate()}/${negociacao.data.getMonth()+1}/${negociacao.data.getFullYear()}</th>
+                        <th>${negociacao.quantidade}</th>
+                        <th>${negociacao.valor}</th>
+                        <th>${negociacao.volume}</th>
+                    </tr>
+                  `
+              ).join()}
+            </tbody>
+
+            <tfoot>
+            </tfoot>
+        </table>
+    `;
+  }
+}
