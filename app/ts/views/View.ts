e8caae52b84b7a41d7faaa2a4863a39d742413e8
juniormoraes <contatomoraesjr@gmail.com>
2021-03-03 18:53:32 -0300

    adicioando scalping, esta quebrando a aplicação

--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,15 +1,21 @@
-
 export abstract class View<T> {
   protected _elemento: JQuery;
+  private _scalping: boolean;
 
-  constructor(seletor: string) {
+  // parâmetro opcional temque ser sempre o ultimo do constructor
+  constructor(seletor: string, scalping?: boolean) {
     this._elemento = $(seletor);
+    this._scalping = scalping
   }
 
   update(model: T) {
+    let template = this.template(model);
+    if(this._scalping)
+      template = template.replace(/<script>[\s\S]*?<\/script>/, '');
+    this._elemento.html(template);
+
     this._elemento.html(this.template(model));
   }
 
   abstract template(model: T): string;
-
 }
