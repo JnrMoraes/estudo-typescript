af2adcbe7e9a47561085035de0f281f51fc8ea29
juniormoraes <contatomoraesjr@gmail.com>
2021-03-04 21:51:54 -0300

    Ajustando mensagem para o desenvolvedor

--- app/js/controllers/NegociacaoController.js
+++ app/js/controllers/NegociacaoController.js
@@ -64,6 +64,8 @@ System.register(["../views/index", "../models/index", "../helpers/decorators/ind
                             .filter((negociacao) => negociacoesJaImportadas.some((jaImportada) => negociacao.ehIgual(jaImportada)))
                             .forEach((negociacao) => this._negociacoes.adiciona(negociacao));
                         this._negociacoesView.update(this._negociacoes);
+                    }).catch(err => {
+                        this._mensagemView.update(err.message);
                     });
                 }
             };
--- app/ts/services/NegociacaoService.ts
+++ app/ts/services/NegociacaoService.ts
@@ -1,23 +1,22 @@
 // app/ts/services/NegociacaoService.ts
 
-import { NegociacaoParcial, Negociacao } from '../models/index';
+import { NegociacaoParcial, Negociacao } from "../models/index";
 
 export class NegociacaoService {
-
-    obterNegociacoes(handler: ResponseHandler): Promise<Negociacao[]> {
-
-        return <Promise<Negociacao[]>> fetch('http://localhost:8080/dados')
-            .then(res => handler(res))
-            .then(res => res.json())
-            .then((dados: NegociacaoParcial[]) => 
-                dados.map(dado => new Negociacao(new Date(), dado.vezes, dado.montante))
-            )
-            .catch(err => console.log(err));
-
-    }
+  obterNegociacoes(handler: ResponseHandler): Promise<Negociacao[]> {
+    return <Promise<Negociacao[]>>fetch("http://localhost:8080/dadosx")
+      .then((res) => handler(res))
+      .then((res) => res.json())
+      .then((dados: NegociacaoParcial[]) =>
+        dados.map(
+          (dado) => new Negociacao(new Date(), dado.vezes, dado.montante)
+        )
+      )
+      .catch((err) => console.log(err));
+    throw new Error("Não foi possivel importar as Negociações");
+  }
 }
 
 export interface ResponseHandler {
-
-  (res: Response): Response
+  (res: Response): Response;
 }
